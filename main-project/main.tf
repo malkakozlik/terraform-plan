f6867c76b208217c36e1f213f90e13e1c638996c
M. Kozlik <malkak@skyvar.co.il>
2024-04-11 14:22:05 +0300

    add

--- main-project/main.tf
+++ main-project/main.tf
@@ -49,7 +49,6 @@ module storages{
   source = "../storages/"
   key_vault_name = module.administrator.key_vault_name
   key_vault_resource_group_name = module.administrator.key_vault_resource_group_name
-  key_vault_secret_excel_name = module.administrator.secret_administrators_name
   DOCKER_REGISTRY_SERVER_URL = var.DOCKER_REGISTRY_SERVER_URL
   DOCKER_REGISTRY_SERVER_USERNAME = var.DOCKER_REGISTRY_SERVER_USERNAME
   DOCKER_REGISTRY_SERVER_PASSWORD = var.DOCKER_REGISTRY_SERVER_PASSWORD
